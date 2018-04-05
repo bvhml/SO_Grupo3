b6b00bb4051c8a3ef1eccbcc1f885733788e09aa
bvhml <victorh-morales@hotmail.com>
2018-04-05 11:17:23 -0700

    Changed SCHED_RR to SCHED_FIFO (Policy).

--- Hellothreads.c
+++ Hellothreads.c
@@ -7,37 +7,32 @@
 
 
 int rc,rc2,rc3,rc4,rc5;
-int newprio = 21;
-int newprio2 = 22;
+int newprio = 25;
+int newprio2 = 24;
 int newprio3 = 23;
-int newprio4 = 24;
-int newprio5 = 25;
+int newprio4 = 22;
+int newprio5 = 21;
 pthread_attr_t attr,attr2,attr3,attr4,attr5;
 struct sched_param param,param2,param3,param4,param5;
 int sum = 0;
 
 void *hello1(void *param) {
-    sleep(3);
     printf("Hello from thread 1 \n");
     return NULL;
 }
 void *hello2(void *param) {
-    sleep(3);
     printf("Hello from thread 2 \n");
     return NULL;
 }
 void *hello3(void *param) {
-    sleep(3);
     printf("Hello from thread 3 \n");
     return NULL;
 }
 void *hello4(void *param) {
-    sleep(3);
     printf("Hello from thread 4 \n");
     return NULL;
 }
 void *hello5(void *param) {
-    sleep(3);
     printf("Hello from thread 5 \n");
     return NULL;
 }
@@ -53,36 +48,38 @@ int main() {
     //Definir prioridades
     rc = pthread_attr_init(&attr);
     rc = pthread_attr_getschedparam(&attr,&param);
+    printf("Prioridad original: %i \n",param.sched_priority);
     param.sched_priority = newprio;
-    rc = pthread_attr_setschedpolicy(&attr, SCHED_RR);
+    rc = pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
     rc = pthread_attr_setschedparam(&attr,&param);
-
+    rc = pthread_attr_getschedparam(&attr,&param);
+    printf("Prioridad modificada: %i \n",param.sched_priority);
     //Definir prioridades
     rc2 = pthread_attr_init(&attr2);
     rc2 = pthread_attr_getschedparam(&attr2,&param2);
     param2.sched_priority = newprio2;
-    rc2 = pthread_attr_setschedpolicy(&attr2, SCHED_RR);
+    rc2 = pthread_attr_setschedpolicy(&attr2, SCHED_FIFO);
     rc2 = pthread_attr_setschedparam(&attr2,&param2);
 
     //Definir prioridades
     rc3 = pthread_attr_init(&attr3);
     rc3 = pthread_attr_getschedparam(&attr3,&param3);
     param3.sched_priority = newprio3;
-    rc3 = pthread_attr_setschedpolicy(&attr3, SCHED_RR);
+    rc3 = pthread_attr_setschedpolicy(&attr3, SCHED_FIFO);
     rc3 = pthread_attr_setschedparam(&attr3,&param3);
 
     //Definir prioridades
     rc4 = pthread_attr_init(&attr4);
     rc4 = pthread_attr_getschedparam(&attr4,&param4);
     param4.sched_priority = newprio4;
-    rc4 = pthread_attr_setschedpolicy(&attr4, SCHED_RR);
+    rc4 = pthread_attr_setschedpolicy(&attr4, SCHED_FIFO);
     rc4 = pthread_attr_setschedparam(&attr4,&param4);
 
     //Definir prioridades
     rc5 = pthread_attr_init(&attr5);
     rc5 = pthread_attr_getschedparam(&attr5,&param5);
     param5.sched_priority = newprio5;
-    rc5 = pthread_attr_setschedpolicy(&attr5, SCHED_RR);
+    rc5 = pthread_attr_setschedpolicy(&attr5, SCHED_FIFO);
     rc5 = pthread_attr_setschedparam(&attr5,&param5);
 
     pthread_create(&tid1, &attr, hello1, NULL);
